4ae9064247f5451c157aa63c40d02f2ea4bb9e05
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2022-12-15 19:14:45 +0000

    d

--- baseProjectExt3/functions/code/src/main/scala/rohitjain25simpledatalabs/com_team/baseprojectext3/functions/package.scala
+++ baseProjectExt3/functions/code/src/main/scala/rohitjain25simpledatalabs/com_team/baseprojectext3/functions/package.scala
@@ -1,4 +1,5 @@
 package rohitjain25simpledatalabs.com_team.baseprojectext3
+import org.apache.spark.sql._
 package object functions {
   val sharedSquare = SharedSquare.sharedSquare
   def registerFunctions(spark: SparkSession) = {
