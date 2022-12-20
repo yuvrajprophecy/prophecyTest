c5c242e819875320e78f639eb0848d1dd3e5b1be
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2022-12-20 07:41:08 +0000

    prophecy conflict resolution for autogenerated files

--- applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2/functions/package.scala
+++ applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2/functions/package.scala
@@ -2,7 +2,7 @@ package simpledatalabs_27.applicationprojectext2
 import org.apache.spark.sql._
 package object functions {
   val square = Square.square
-  def registerFunctions(spark: SparkSession) = { oueee
+  def registerFunctions(spark: SparkSession) = {
     spark.udf.register("square", square)
   }
 }
