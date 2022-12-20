a479b828560828e0ca696aea656317cdda19d210
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2022-12-20 08:30:17 +0000

    added udf

--- applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2/functions/package.scala
+++ applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2/functions/package.scala
@@ -2,9 +2,12 @@ package simpledatalabs_27.applicationprojectext2
 
 import org.apache.spark.sql._
 package object functions {
-  val square = Square.square
+  val square  = Square.square
+  val square2 = Square2.square2
 
-  def registerFunctions(spark: SparkSession) =
-    spark.udf.register("square", square)
+  def registerFunctions(spark: SparkSession) = {
+    spark.udf.register("square",  square)
+    spark.udf.register("square2", square2)
+  }
 
 }
---
+++ applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2/functions/square2.scala
@@ -0,0 +1,11 @@
+package simpledatalabs_27.applicationprojectext2.functions
+
+import _root_.io.prophecy.abinitio.ScalaFunctions._
+import _root_.io.prophecy.libs._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
+
+object Square2 extends Serializable {
+  def square2 = udf((i2: Int) => i2 * i2)
+}
