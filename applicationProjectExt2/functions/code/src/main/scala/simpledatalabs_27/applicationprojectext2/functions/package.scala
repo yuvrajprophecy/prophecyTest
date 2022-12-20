897776b31a3f61490df811abacfafae5f10b99cd
Rohit Jain <jain.rohit625@gmail.com>
2022-12-20 13:10:05 +0530

    Update package.scala

--- applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2/functions/package.scala
+++ applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2/functions/package.scala
@@ -2,7 +2,7 @@ package simpledatalabs_27.applicationprojectext2
 import org.apache.spark.sql._
 package object functions {
   val square = Square.square
-  def registerFunctions(spark: SparkSession) = {
+  def registerFunctions(spark: SparkSession) = { oueee
     spark.udf.register("square", square)
   }
 }
