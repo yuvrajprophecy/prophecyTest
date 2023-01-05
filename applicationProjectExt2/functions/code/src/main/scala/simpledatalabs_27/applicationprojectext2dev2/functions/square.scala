300b31c3ccac4ec733c8402021523356279b3ff1
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-01-05 07:28:11 +0000

    d

--- applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2dev2/functions/square.scala
+++ applicationProjectExt2/functions/code/src/main/scala/simpledatalabs_27/applicationprojectext2dev2/functions/square.scala
@@ -5,4 +5,8 @@ import _root_.io.prophecy.libs._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql._
-object Square extends Serializable { def square = udf((i: Int) => i * i) }
+
+object Square extends Serializable {
+  val x      = 10
+  def square = udf((i: Int) => i * i)
+}
