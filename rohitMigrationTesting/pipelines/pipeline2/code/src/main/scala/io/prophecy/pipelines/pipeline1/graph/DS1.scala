25d7942bee61765edbaa3a49ec3d13372f72a6d6
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-02-01 12:56:41 +0000

    s

--- rohitMigrationTesting/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/graph/DS1.scala
+++ rohitMigrationTesting/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/graph/DS1.scala
@@ -9,11 +9,15 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.expressions._
 import java.time._
-package object Subgraph_1 {
 
-  def apply(context: Context, in0: DataFrame): DataFrame = {
-    val df_Reformat_2 = Reformat_2(context, in0)
-    df_Reformat_2
-  }
+object DS1 {
+
+  def apply(context: Context): DataFrame =
+    context.spark.read
+      .format("csv")
+      .option("header",                                   true)
+      .option("sep",                                      ",")
+      .schema(StructType(Array(StructField("customer_id", StringType, true))))
+      .load("dede")
 
 }
--- rohitMigrationTesting/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/graph/Subgraph_1/Reformat_2.scala
+++ rohitMigrationTesting/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/graph/Subgraph_1/Reformat_2.scala
@@ -1,19 +1,14 @@
-package io.prophecy.pipelines.pipeline1.graph
+package io.prophecy.pipelines.pipeline1.graph.Subgraph_1
 
 import io.prophecy.libs._
 import io.prophecy.pipelines.pipeline1.config.ConfigStore._
 import io.prophecy.pipelines.pipeline1.config.Context
+import io.prophecy.pipelines.pipeline1.udfs.UDFs._
+import io.prophecy.pipelines.pipeline1.udfs._
 import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.expressions._
 import java.time._
-package object Subgraph_1 {
-
-  def apply(context: Context, in0: DataFrame): DataFrame = {
-    val df_Reformat_2 = Reformat_2(context, in0)
-    df_Reformat_2
-  }
-
-}
+object Reformat_2 { def apply(context: Context, in: DataFrame): DataFrame = in }
