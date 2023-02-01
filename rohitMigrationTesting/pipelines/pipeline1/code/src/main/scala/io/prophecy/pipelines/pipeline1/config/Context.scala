a03c20b6061893a63aa0bb1221d6121f8759c345
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-02-01 13:03:27 +0000

    d

--- rohitMigrationTesting/pipelines/pipeline1/code/src/main/scala/io/prophecy/pipelines/pipeline1/config/Context.scala
+++
@@ -1,4 +0,0 @@
-package io.prophecy.pipelines.pipeline1.config
-
-import org.apache.spark.sql.SparkSession
-case class Context(spark: SparkSession, config: Config)
---
+++ rohitMigrationTesting/pipelines/pipeline1/code/src/main/scala/simpledatalabs_27/rohitmigrationtesting/pipeline/pipeline1/graph/Subgraph_1/package.scala
@@ -0,0 +1,13 @@
+package simpledatalabs_27.rohitmigrationtesting.pipeline.pipeline1.graph
+
+import io.prophecy.libs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+import simpledatalabs_27.rohitmigrationtesting.pipeline.pipeline1.graph.Subgraph_1.config._
+package object Subgraph_1 {
+  def apply(context: Context, in0: DataFrame): Unit = {}
+}
