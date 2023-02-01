e419c765259ead6e2998d9079ce0a32b4ded8739
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-02-01 14:59:23 +0000

    ss

--- rohitMigrationTesting/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/config/Config.scala
+++
@@ -1,8 +0,0 @@
-package io.prophecy.pipelines.pipeline1.config
-
-import io.prophecy.pipelines.pipeline1.config.ConfigStore._
-import io.prophecy.pipelines.pipeline1.config.Context
-import pureconfig._
-import pureconfig.generic.ProductHint
-import io.prophecy.libs._
-case class Config() extends ConfigBase
