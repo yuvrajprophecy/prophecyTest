28160af4d2d02f0b66294d46c691b1338a1cd975
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-02-20 16:55:16 +0000

    d

--- ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/io/prophecy/pipelines/pipeline1/udfs/UDFs.scala
+++ ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/io/prophecy/pipelines/pipeline1/udfs/UDFs.scala
@@ -11,6 +11,7 @@ object UDFs extends Serializable {
   def registerUDFs(spark: SparkSession) = {
     spark.udf.register("square",  square)
     spark.udf.register("trimUDF", trimUDF)
+    spark.udf.register("square2", square2)
   }
 
   def square = {
@@ -22,6 +23,9 @@ object UDFs extends Serializable {
     val y = 10
     udf((value: String) => value.trim())
   }
+
+  def square2 =
+    udf((value2: String) => value2 + value2)
 
 }
 
