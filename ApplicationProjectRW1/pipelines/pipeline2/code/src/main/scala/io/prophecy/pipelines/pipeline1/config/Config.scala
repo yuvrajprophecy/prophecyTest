320c7363ab0fa55291a51799be28c68acd354ea5
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-01 07:06:46 +0000

    d

--- ApplicationProjectRW1/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/config/Config.scala
+++ ApplicationProjectRW1/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/config/Config.scala
@@ -1,24 +1,22 @@
 package io.prophecy.pipelines.pipeline1.config
 
-import io.prophecy.pipelines.pipeline1.config.ConfigStore._
-import io.prophecy.pipelines.pipeline1.config.Context
 import pureconfig._
 import pureconfig.generic.ProductHint
 import io.prophecy.libs._
 
 case class Config(
-  Subgraph_1: Subgraph_1 = Subgraph_1(),
+  Subgraph_1: SUBGRAPH_1 = SUBGRAPH_1(),
   basesg1_1:  Basesg1_1 = Basesg1_1()
 ) extends ConfigBase
 
-object Subgraph_1 {
+object SUBGRAPH_1 {
 
-  implicit val confHint: ProductHint[Subgraph_1] =
-    ProductHint[Subgraph_1](ConfigFieldMapping(CamelCase, CamelCase))
+  implicit val confHint: ProductHint[SUBGRAPH_1] =
+    ProductHint[SUBGRAPH_1](ConfigFieldMapping(CamelCase, CamelCase))
 
 }
 
-case class Subgraph_1()
+case class SUBGRAPH_1()
 
 object Basesg1_1 {
 
--- ApplicationProjectRW1/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/main.scala
+++ ApplicationProjectRW1/pipelines/pipeline2/code/src/main/scala/io/prophecy/pipelines/pipeline1/main.scala
@@ -1,7 +1,6 @@
 package io.prophecy.pipelines.pipeline1
 
 import io.prophecy.libs._
-import io.prophecy.pipelines.pipeline1.config.ConfigStore._
 import io.prophecy.pipelines.pipeline1.config.Context
 import io.prophecy.pipelines.pipeline1.config._
 import io.prophecy.pipelines.pipeline1.udfs.UDFs._
@@ -33,7 +32,8 @@ object Main {
       .newSession()
     val context = Context(spark, config)
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/pipeline2")
-    MetricsCollector.start(spark,                    "pipelines/pipeline2")
+    registerUDFs(spark)
+    MetricsCollector.start(spark, "pipelines/pipeline2")
     apply(context)
     MetricsCollector.end(spark)
   }
