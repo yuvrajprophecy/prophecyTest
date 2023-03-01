d3e96956220b7ec728e2cd6ebcc7d1eb4beeea59
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-01 07:10:48 +0000

    d

--- ApplicationProjectRW1/functions/code/src/main/scala/rohitjain27simpledatalabs/com_team/pm3rework/functions/package.scala
+++ ApplicationProjectRW1/functions/code/src/main/scala/rohitjain27simpledatalabs/com_team/pm3rework/functions/package.scala
@@ -4,10 +4,12 @@ import org.apache.spark.sql._
 package object functions {
   val trimUDF  = TrimUDF.trimUDF
   val trimUDF2 = TrimUDF2.trimUDF2
+  val trimUDF3 = TrimUDF3.trimUDF3
 
   def registerFunctions(spark: SparkSession) = {
     spark.udf.register("trimUDF",  trimUDF)
     spark.udf.register("trimUDF2", trimUDF2)
+    spark.udf.register("trimUDF3", trimUDF3)
   }
 
 }
