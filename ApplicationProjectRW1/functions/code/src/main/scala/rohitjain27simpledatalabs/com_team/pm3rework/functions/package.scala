74e1f04d054bf04efed7cd26720ef0e97c9061f0
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-13 06:15:17 +0000

    ❌ Deleted     - UDF        - trimUDF1
    🔁 Modified
        - Pipeline
           - pipeline1
        - UDF
           - trimUDF
           - trimUDF22
           - trimUDF2
        - Subgraph
           - sg1

--- ApplicationProjectRW1/functions/code/src/main/scala/rohitjain27simpledatalabs/com_team/pm3rework/functions/package.scala
+++ ApplicationProjectRW1/functions/code/src/main/scala/rohitjain27simpledatalabs/com_team/pm3rework/functions/package.scala
@@ -1,17 +1,4 @@
 package rohitjain27simpledatalabs.com_team.pm3rework
 
 import org.apache.spark.sql._
-package object functions {
-  val trimUDF2  = TrimUDF2.trimUDF2
-  val trimUDF   = TrimUDF.trimUDF
-  val trimUDF1  = TrimUDF1.trimUDF1
-  val trimUDF22 = TrimUDF22.trimUDF22
-
-  def registerFunctions(spark: SparkSession) = {
-    spark.udf.register("trimUDF2",  trimUDF2)
-    spark.udf.register("trimUDF",   trimUDF)
-    spark.udf.register("trimUDF1",  trimUDF1)
-    spark.udf.register("trimUDF22", trimUDF22)
-  }
-
-}
+package object functions { def registerFunctions(spark: SparkSession) = {} }
--- ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/io/prophecy/pipelines/pipeline1/udfs/UDFs.scala
+++ ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/io/prophecy/pipelines/pipeline1/udfs/UDFs.scala
@@ -8,33 +8,8 @@ import org.apache.spark.sql._
 
 object UDFs extends Serializable {
 
-  def registerUDFs(spark: SparkSession) = {
-    spark.udf.register("trimUDF2",       trimUDF2)
-    spark.udf.register("trimUDF",        trimUDF)
-    spark.udf.register("trimUDF1",       trimUDF1)
-    spark.udf.register("trimUDF22",      trimUDF22)
+  def registerUDFs(spark: SparkSession) =
     spark.udf.register("createFullName", createFullName)
-  }
-
-  def trimUDF2 = {
-    val x = 3
-    udf((value3: String) => value3.trim())
-  }
-
-  def trimUDF = {
-    val x = 0
-    udf((value: String) => value.trim())
-  }
-
-  def trimUDF1 = {
-    val x = 1
-    udf((value1: String) => value1.trim())
-  }
-
-  def trimUDF22 = {
-    val x = 22
-    udf((value22: String) => value22.trim())
-  }
 
   def createFullName =
     udf((value1: String, value2: String) => value1 + value2)
