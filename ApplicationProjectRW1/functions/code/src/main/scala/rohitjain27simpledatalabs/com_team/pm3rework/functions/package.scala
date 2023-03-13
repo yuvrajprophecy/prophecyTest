0aa582f6fad756dfb3f9318ecf6fad2959ea8ba0
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-13 05:54:15 +0000

    🔁 Modified     - UDF        - trimUDF4     - Subgraph        - sg1     - Pipeline        - pipeline1
    ❌ Deleted
        - UDF
           - trimUDF4
           - trimUDF4

--- ApplicationProjectRW1/functions/code/src/main/scala/rohitjain27simpledatalabs/com_team/pm3rework/functions/package.scala
+++ ApplicationProjectRW1/functions/code/src/main/scala/rohitjain27simpledatalabs/com_team/pm3rework/functions/package.scala
@@ -1,13 +1,4 @@
 package rohitjain27simpledatalabs.com_team.pm3rework
 
 import org.apache.spark.sql._
-package object functions {
-  val trimUDF4 = TrimUDF4.trimUDF4
-  val trimUDF  = TrimUDF.trimUDF
-
-  def registerFunctions(spark: SparkSession) = {
-    spark.udf.register("trimUDF4", trimUDF4)
-    spark.udf.register("trimUDF",  trimUDF)
-  }
-
-}
+package object functions { def registerFunctions(spark: SparkSession) = {} }
--- ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/io/prophecy/pipelines/pipeline1/udfs/UDFs.scala
+++ ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/io/prophecy/pipelines/pipeline1/udfs/UDFs.scala
@@ -8,21 +8,8 @@ import org.apache.spark.sql._
 
 object UDFs extends Serializable {
 
-  def registerUDFs(spark: SparkSession) = {
-    spark.udf.register("trimUDF",        trimUDF)
-    spark.udf.register("trimUDF4",       trimUDF4)
+  def registerUDFs(spark: SparkSession) =
     spark.udf.register("createFullName", createFullName)
-  }
-
-  def trimUDF = {
-    val pipeline1 = "ldme"
-    udf((value1: String) => value1.trim())
-  }
-
-  def trimUDF4 = {
-    val pipeline3 = "ldme"
-    udf((value3: String) => value3.trim())
-  }
 
   def createFullName =
     udf((value1: String, value2: String) => value1 + value2)
