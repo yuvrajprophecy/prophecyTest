92a4801b47494a5fa11c6855b7bf97ef5c0b713f
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-21 17:40:46 +0000

    ❌ Deleted     - UDF        - createFullName2        - createFullName1
    🔁 Modified
        - Pipeline
           - basePipeline1
        - UDF
           - createFullName

--- baseProjectRW/functions/code/src/main/scala/simpledatalabs_27/baseprojectrw/functions/package.scala
+++ baseProjectRW/functions/code/src/main/scala/simpledatalabs_27/baseprojectrw/functions/package.scala
@@ -1,15 +1,4 @@
 package simpledatalabs_27.baseprojectrw
 
 import org.apache.spark.sql._
-package object functions {
-  val createFullName2 = CreateFullName2.createFullName2
-  val createFullName1 = CreateFullName1.createFullName1
-  val createFullName  = CreateFullName.createFullName
-
-  def registerFunctions(spark: SparkSession) = {
-    spark.udf.register("createFullName2", createFullName2)
-    spark.udf.register("createFullName1", createFullName1)
-    spark.udf.register("createFullName",  createFullName)
-  }
-
-}
+package object functions { def registerFunctions(spark: SparkSession) = {} }
--- baseProjectRW/pipelines/basePipeline1/code/src/main/scala/io/prophecy/pipelines/basepipeline1/udfs/UDFs.scala
+++ baseProjectRW/pipelines/basePipeline1/code/src/main/scala/io/prophecy/pipelines/basepipeline1/udfs/UDFs.scala
@@ -8,27 +8,8 @@ import org.apache.spark.sql._
 
 object UDFs extends Serializable {
 
-  def registerUDFs(spark: SparkSession) = {
-    spark.udf.register("createFullName2", createFullName2)
-    spark.udf.register("createFullName1", createFullName1)
-    spark.udf.register("createFullName",  createFullName)
+  def registerUDFs(spark: SparkSession) =
     registerAllUDFs(spark)
-  }
-
-  def createFullName2 = {
-    val x = 3
-    udf((value1: String, value2: String) => value1 + value2)
-  }
-
-  def createFullName1 = {
-    val x = 4
-    udf((value1: String, value2: String) => value1 + value2)
-  }
-
-  def createFullName = {
-    val x = 5
-    udf((value1: String, value2: String) => value1 + value2)
-  }
 
 }
 
