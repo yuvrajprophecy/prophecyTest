4bc666545c0a7faeb7677e5a844c6104ce49e90a
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-22 06:01:47 +0000

    🔁 Modified     - UDF        - createFullName5        - createFullName3        - createFullName2        - createFullName1        - createFullName7     - Pipeline        - basePipeline2
    ❇️ New
        - UDF
           - createFullName8

--- baseProjectRW/pipelines/basePipeline2/code/src/main/scala/io/prophecy/pipelines/basepipeline2/udfs/UDFs.scala
+++ baseProjectRW/pipelines/basePipeline2/code/src/main/scala/io/prophecy/pipelines/basepipeline2/udfs/UDFs.scala
@@ -9,36 +9,12 @@ import org.apache.spark.sql._
 object UDFs extends Serializable {
 
   def registerUDFs(spark: SparkSession) = {
-    spark.udf.register("createFullName1", createFullName1)
-    spark.udf.register("createFullName2", createFullName2)
-    spark.udf.register("createFullName3", createFullName3)
-    spark.udf.register("createFullName5", createFullName5)
-    spark.udf.register("createFullName7", createFullName7)
+    spark.udf.register("createFullName8", createFullName8)
     registerAllUDFs(spark)
   }
 
-  def createFullName1 = {
-    val x = 1
-    udf((v1: String, v2: String) => v1 + v2)
-  }
-
-  def createFullName2 = {
-    val x = 2
-    udf((v1: String, v2: String) => v1 + v2)
-  }
-
-  def createFullName3 = {
-    val x = 3
-    udf((v1: String, v2: String) => v1 + v2)
-  }
-
-  def createFullName5 = {
-    val x = 5
-    udf((v1: String, v2: String) => v1 + v2)
-  }
-
-  def createFullName7 = {
-    val x = 7
+  def createFullName8 = {
+    val x = 8
     udf((v1: String, v2: String) => v1 + v2)
   }
 
