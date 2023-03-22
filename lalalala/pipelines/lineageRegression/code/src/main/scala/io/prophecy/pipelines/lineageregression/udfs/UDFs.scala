d22f1e38ca535d89d0de4a28aeb0486172cea029
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-22 11:03:01 +0000

    🔁 Modified     - Pipeline        - lineageRegression     - UDF        - generate_vector        - generate_matrix        - matrix_col_count

--- lalalala/pipelines/lineageRegression/code/src/main/scala/io/prophecy/pipelines/lineageregression/udfs/UDFs.scala
+++ lalalala/pipelines/lineageRegression/code/src/main/scala/io/prophecy/pipelines/lineageregression/udfs/UDFs.scala
@@ -9,33 +9,25 @@ import org.apache.spark.sql._
 object UDFs extends Serializable {
 
   def registerUDFs(spark: SparkSession) = {
-    spark.udf.register("vector_to_array",  vector_to_array)
-    spark.udf.register("generate_vector",  generate_vector)
-    spark.udf.register("generate_matrix",  generate_matrix)
     spark.udf.register("matrix_col_count", matrix_col_count)
+    spark.udf.register("generate_matrix",  generate_matrix)
+    spark.udf.register("generate_vector",  generate_vector)
     registerAllUDFs(spark)
   }
 
-  def vector_to_array = {
-    import org.apache.spark.mllib.linalg.Vector
-    udf((value: Vector) => value.toArray)
+  def matrix_col_count = {
+    import org.apache.spark.ml.linalg.Matrix
+    udf((mat: Matrix) => mat.numCols)
+  }
+
+  def generate_matrix = {
+    import org.apache.spark.ml.linalg.Matrices
+    udf(() => Matrices.dense(2, 2, Array(1, 2, 3, 4)))
   }
 
   def generate_vector = {
     import org.apache.spark.ml.linalg.Vectors
-    udf(() => Vectors.dense(0.1d, 0.2d, 0.3d))
-  }
-
-  def generate_matrix = {
-    import org.apache.spark.ml.linalg.{Matrices, Matrix}
-    udf { () =>
-      Matrices.dense(3, 2, Array(1.0d, 3.0d, 5.0d, 2.0d, 4.0d, 6.0d))
-    }
-  }
-
-  def matrix_col_count = {
-    import org.apache.spark.ml.linalg.{Matrices, Matrix}
-    udf((mat: Matrix) => mat.numCols)
+    udf(() => Vectors.dense(1.0d, 2.0d))
   }
 
 }
