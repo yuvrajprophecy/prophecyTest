ddd778dc955934ba8ba69c084933c532bb29351b
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-22 11:09:06 +0000

    ❌ Deleted     - UDF        - test_udf
    🔁 Modified
        - Pipeline
           - lineageRegression
           - pppp

--- lalalala/pipelines/pppp/code/src/main/scala/udfs/UDFs.scala
+++ lalalala/pipelines/pppp/code/src/main/scala/udfs/UDFs.scala
@@ -8,8 +8,27 @@ import org.apache.spark.sql._
 
 object UDFs extends Serializable {
 
-  def registerUDFs(spark: SparkSession) =
+  def registerUDFs(spark: SparkSession) = {
+    spark.udf.register("matrix_col_count", matrix_col_count)
+    spark.udf.register("generate_matrix",  generate_matrix)
+    spark.udf.register("generate_vector",  generate_vector)
     registerAllUDFs(spark)
+  }
+
+  def matrix_col_count = {
+    import org.apache.spark.ml.linalg.Matrix
+    udf((mat: Matrix) => mat.numCols)
+  }
+
+  def generate_matrix = {
+    import org.apache.spark.ml.linalg.Matrices
+    udf(() => Matrices.dense(2, 2, Array(1, 2, 3, 4)))
+  }
+
+  def generate_vector = {
+    import org.apache.spark.ml.linalg.Vectors
+    udf(() => Vectors.dense(1.0d, 2.0d))
+  }
 
 }
 
