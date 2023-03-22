4d771f1d049ba1dc383da728fb018fb0073821a2
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-22 05:57:35 +0000

    ❇️ New     - UDF        - createFullName5        - createFullName4
    🔁 Modified
        - Pipeline
           - basePipeline2
           - basePipeline1
        - UDF
           - createFullName3

--- baseProjectRW/pipelines/basePipeline2/code/src/main/scala/io/prophecy/pipelines/basepipeline2/udfs/UDFs.scala
+++ baseProjectRW/pipelines/basePipeline2/code/src/main/scala/io/prophecy/pipelines/basepipeline2/udfs/UDFs.scala
@@ -11,7 +11,8 @@ object UDFs extends Serializable {
   def registerUDFs(spark: SparkSession) = {
     spark.udf.register("createFullName1", createFullName1)
     spark.udf.register("createFullName2", createFullName2)
-    spark.udf.register("createFullName4", createFullName4)
+    spark.udf.register("createFullName3", createFullName3)
+    spark.udf.register("createFullName5", createFullName5)
     registerAllUDFs(spark)
   }
 
@@ -25,8 +26,13 @@ object UDFs extends Serializable {
     udf((v1: String, v2: String) => v1 + v2)
   }
 
-  def createFullName4 = {
+  def createFullName3 = {
     val x = 4
+    udf((v1: String, v2: String) => v1 + v2)
+  }
+
+  def createFullName5 = {
+    val x = 5
     udf((v1: String, v2: String) => v1 + v2)
   }
 
