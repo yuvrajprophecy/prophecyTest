c02905f58ef060caff864346d649b4900d50a411
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-23 08:02:01 +0000

    ❇️ New     - UDF        - udf5        - udf1        - udf2        - udf4        - udf3
    🔁 Modified
        - Pipeline
           - frfrf

--- lalalala/pipelines/frfrf/code/src/main/scala/io/prophecy/pipelines/frfrf/udfs/UDFs.scala
+++ lalalala/pipelines/frfrf/code/src/main/scala/io/prophecy/pipelines/frfrf/udfs/UDFs.scala
@@ -12,6 +12,11 @@ object UDFs extends Serializable {
     spark.udf.register("matrix_col_count", matrix_col_count)
     spark.udf.register("generate_matrix",  generate_matrix)
     spark.udf.register("generate_vector",  generate_vector)
+    spark.udf.register("udf1",             udf1)
+    spark.udf.register("udf2",             udf2)
+    spark.udf.register("udf3",             udf3)
+    spark.udf.register("udf4",             udf4)
+    spark.udf.register("udf5",             udf5)
     registerAllUDFs(spark)
   }
 
@@ -28,6 +33,31 @@ object UDFs extends Serializable {
   def generate_vector = {
     import org.apache.spark.ml.linalg.Vectors
     udf(() => Vectors.dense(1.0d, 2.0d))
+  }
+
+  def udf1 = {
+    val x = 1
+    udf(() => "1")
+  }
+
+  def udf2 = {
+    val x = 2
+    udf(() => "2")
+  }
+
+  def udf3 = {
+    val x = 3
+    udf(() => "3")
+  }
+
+  def udf4 = {
+    val x = 4
+    udf(() => "4")
+  }
+
+  def udf5 = {
+    val x = 5
+    udf(() => "5")
   }
 
 }
