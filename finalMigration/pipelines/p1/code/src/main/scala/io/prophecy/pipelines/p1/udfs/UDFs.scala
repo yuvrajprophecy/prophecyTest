0fbe65e902c905608c97268d1883d2500a1f60a5
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-23 09:08:09 +0000

    ❌ Deleted     - UDF        - udf12        - udf32        - udf21
    🔁 Modified
        - Pipeline
           - p1

--- finalMigration/pipelines/p1/code/src/main/scala/io/prophecy/pipelines/p1/udfs/UDFs.scala
+++ finalMigration/pipelines/p1/code/src/main/scala/io/prophecy/pipelines/p1/udfs/UDFs.scala
@@ -8,27 +8,8 @@ import org.apache.spark.sql._
 
 object UDFs extends Serializable {
 
-  def registerUDFs(spark: SparkSession) = {
-    spark.udf.register("udf12", udf12)
-    spark.udf.register("udf21", udf21)
-    spark.udf.register("udf32", udf32)
+  def registerUDFs(spark: SparkSession) =
     registerAllUDFs(spark)
-  }
-
-  def udf12 = {
-    val x = 3
-    udf(() => "3")
-  }
-
-  def udf21 = {
-    val x = 3
-    udf(() => "3")
-  }
-
-  def udf32 = {
-    val x = 3
-    udf(() => "3")
-  }
 
 }
 
