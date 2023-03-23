902f291d44ebd37c0c22d613d09eb4747289a8a9
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-23 06:14:56 +0000

    d

---
+++ finalMigration/pipelines/p3/code/src/main/scala/io/prophecy/pipelines/p1/udfs/UDFs.scala
@@ -0,0 +1,21 @@
+package io.prophecy.pipelines.p1.udfs
+
+import _root_.io.prophecy.abinitio.ScalaFunctions._
+import _root_.io.prophecy.libs._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
+
+object UDFs extends Serializable {
+  val x = 3
+
+  def registerUDFs(spark: SparkSession) = {
+    spark.udf.register("udf1", udf1)
+    spark.udf.register("udf2", udf2)
+    spark.udf.register("udf3", udf3)
+  }
+
+  def udf1 = udf(() => "")
+  def udf2 = udf(() => "")
+  def udf3 = udf(() => "")
+}
