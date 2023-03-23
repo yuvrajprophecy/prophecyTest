870d1a3e0885f7793eab5f1a19df224f29bdfd1b
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-03-23 08:39:47 +0000

    🔁 Modified     - Pipeline        - p3     - UDF        - udf2        - udf1        - udf3

--- finalMigration/pipelines/p3/code/src/main/scala/io/prophecy/pipelines/p1/config/ConfigStore.scala
+++ finalMigration/pipelines/p3/code/src/main/scala/io/prophecy/pipelines/p1/config/ConfigStore.scala
@@ -1,6 +1,5 @@
 package io.prophecy.pipelines.p1.config
 
-import io.prophecy.pipelines.p1.config.ConfigStore._
 import io.prophecy.pipelines.p1.config.Context
 import pureconfig.ConfigReader.Result
 import pureconfig._
