aa6b2ffbf3a63608647ab66e840332ca506d3d9f
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-04-14 09:43:07 +0000

    ❌ Deleted     - Pipeline        - p3        - p1

--- MigrationPy/pipelines/p1/code/p1/graph/Subgraph_2/Reformat_3.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from prophecy.libs import typed_lit
-from .config import *
-from p1.udfs.UDFs import *
-
-def Reformat_3(spark: SparkSession, in0: DataFrame) -> DataFrame:
-    return in0
