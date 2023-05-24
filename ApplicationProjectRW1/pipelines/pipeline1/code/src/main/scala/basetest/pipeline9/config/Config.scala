f820f67b6b7d8a56c3d6fc3a0047e2305810fcb7
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-05-24 11:22:59 +0000

    🔁 Modified     - Pipeline        - pipeline1        - pipeline2        - pipeline3        - kaajri12312

--- ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/basetest/pipeline9/config/Config.scala
+++ ApplicationProjectRW1/pipelines/pipeline1/code/src/main/scala/basetest/pipeline9/config/Config.scala
@@ -3,4 +3,23 @@ package basetest.pipeline9.config
 import pureconfig._
 import pureconfig.generic.ProductHint
 import io.prophecy.libs._
-case class Config() extends ConfigBase
+
+case class Config(
+  boolean_config: Boolean = true,
+  string_config:  String = "default_str",
+  double_config:  Double = 1.0d,
+  array_config:   List[String] = List("1"),
+  record_config:  Record_config = Record_config()
+) extends ConfigBase
+
+object Record_config {
+
+  implicit val confHint: ProductHint[Record_config] =
+    ProductHint[Record_config](ConfigFieldMapping(CamelCase, CamelCase))
+
+}
+
+case class Record_config(
+  record_string_config: String = "record_string_config",
+  record_float_config:  Float = 2.0f
+)
