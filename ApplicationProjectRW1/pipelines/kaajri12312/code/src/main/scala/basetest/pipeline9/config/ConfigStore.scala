99dd8b6506c1428ac1b2dc6539792190a1a0277e
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2023-07-20 07:46:05 +0000

    🔁 Modified     - Pipeline        - kaajri12312

--- ApplicationProjectRW1/pipelines/kaajri12312/code/src/main/scala/basetest/pipeline9/config/ConfigStore.scala
+++ ApplicationProjectRW1/pipelines/kaajri12312/code/src/main/scala/basetest/pipeline9/config/ConfigStore.scala
@@ -1,16 +1,12 @@
 package basetest.pipeline9.config
 
-import org.apache.spark.sql._
 import basetest.pipeline9.config.Context
 import pureconfig.ConfigReader.Result
 import pureconfig._
 import pureconfig.generic.ProductHint
 import pureconfig.generic.auto._
 import io.prophecy.libs._
-
-object ConfigStore {
-  implicit val interimOutput: InterimOutput = InterimOutputHive2("")
-}
+object ConfigStore
 
 object ConfigurationFactoryImpl extends ConfigurationFactory[Config] {
 
--- ApplicationProjectRW1/pipelines/kaajri12312/code/src/main/scala/basetest/pipeline9/main.scala
+++ ApplicationProjectRW1/pipelines/kaajri12312/code/src/main/scala/basetest/pipeline9/main.scala
@@ -3,7 +3,6 @@ package basetest.pipeline9
 import io.prophecy.libs._
 import basetest.pipeline9.config.Context
 import basetest.pipeline9.config._
-import basetest.pipeline9.config.ConfigStore.interimOutput
 import basetest.pipeline9.udfs.UDFs._
 import basetest.pipeline9.udfs._
 import basetest.pipeline9.udfs.PipelineInitCode._
@@ -17,19 +16,9 @@ import java.time._
 
 object Main {
 
-  def graph(context: Context): Unit = {
-    val df_ds1 = ds1(context).interim(
-      "graph",
-      "RYQnrtN3ocY6_boAf7-HX$$2kX7OLlbNvV4-07_cW-kx",
-      "7HgZqmV9hcpmDeE3tJeXC$$f_L2YxzqZoneVuRsUJHdF"
-    )
-    val df_Reformat_1 = Reformat_1(context, df_ds1).interim(
-      "graph",
-      "JyDMsNrosYkFO0fDZFN_e$$JZFXLetzwrJcBYNfJAtU4",
-      "KzTT7eEaOo_qqVQ0jYPBG$$rCMVi17g-CG1I1neWJJd1"
-    )
-    df_Reformat_1.cache().count()
-    df_Reformat_1.unpersist()
+  def apply(context: Context): Unit = {
+    val df_ds1        = ds1(context)
+    val df_Reformat_1 = Reformat_1(context, df_ds1)
   }
 
   def main(args: Array[String]): Unit = {
@@ -43,20 +32,14 @@ object Main {
       .getOrCreate()
       .newSession()
     val context = Context(spark, config)
-    MetricsCollector.initializeMetrics(spark)
-    implicit val interimOutputConsole: InterimOutput = InterimOutputHive2("")
-    spark.conf.set("prophecy.collect.basic.stats",          "true")
-    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
-    spark.conf.set("spark.sql.optimizer.excludedRules",
-                   "org.apache.spark.sql.catalyst.optimizer.ColumnPruning"
-    )
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/kaajri12312")
-    try MetricsCollector.start(spark,                "pipelines/kaajri12312", context.config)
+    registerUDFs(spark)
+    try MetricsCollector.start(spark, "pipelines/kaajri12312", context.config)
     catch {
       case _: Throwable =>
         MetricsCollector.start(spark, "pipelines/kaajri12312")
     }
-    graph(context)
+    apply(context)
     MetricsCollector.end(spark)
   }
 
