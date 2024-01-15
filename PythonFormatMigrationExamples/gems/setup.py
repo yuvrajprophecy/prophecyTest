4356c6660447c60b718270ef77545ceedc37be34
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2024-01-15 13:43:00 +0000

    [Prophecy] Release version 30001

--- PythonFormatMigrationExamples/gems/setup.py
+++ PythonFormatMigrationExamples/gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'rohitjainprophecyioteam_pythonformatmigrationexamples',
-    version = '0.0.1',
+    version = '30001',
     packages = packages_to_include,
     description = '',
     install_requires = [],
