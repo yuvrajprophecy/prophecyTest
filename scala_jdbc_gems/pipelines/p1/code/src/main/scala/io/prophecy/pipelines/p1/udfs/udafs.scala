39fd381a6cf0dc5ed4d5c7a9bad4c73a376bcf29
jain.rohit625@gmail.com <jain.rohit625@gmail.com>
2024-01-19 12:35:14 +0000

    ❌ Deleted     - Pipeline        - p1

--- scala_jdbc_gems/pipelines/p1/code/src/main/scala/io/prophecy/pipelines/p1/udfs/udafs.scala
+++
@@ -1,6 +0,0 @@
-package io.prophecy.pipelines.p1.udfs
-
-import org.apache.spark.sql.expressions._
-import org.apache.spark.sql.types._
-import org.apache.spark.sql._
-object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
