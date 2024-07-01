8128db5e4166d7441189e5f49605af5955b57d6a
yuvraj@prophecy.io <yuvraj@prophecy.io>
2024-07-01 09:24:21 +0000

    Reverted to Updates prophecy project metadata, commit hash : 7f9609eefe0ce9218c258dff84ebed63509b2ae5

--- plibsTest/pipelines/p2/code/p1/pipeline.py
+++ plibsTest/pipelines/p2/code/p1/pipeline.py
@@ -1,6 +1,7 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from prophecy.utils import *
 from p1.config.ConfigStore import *
 from p1.udfs.UDFs import *
 from prophecy.utils import *
@@ -17,12 +18,21 @@ def main():
                 .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
                 .enableHiveSupport()\
                 .appName("Prophecy Pipeline")\
-                .getOrCreate()
+                .getOrCreate()\
+                .newSession()
     Utils.initializeFromArgs(spark, parse_args())
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/p2")
     registerUDFs(spark)
-    
-    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/p2", config = Config)(pipeline)
+
+    try:
+        
+        MetricsCollector.start(spark = spark, pipelineId = "pipelines/p2", config = Config)
+    except :
+        
+        MetricsCollector.start(spark = spark, pipelineId = "pipelines/p2")
+
+    pipeline(spark)
+    MetricsCollector.end(spark)
 
 if __name__ == "__main__":
     main()
